3f48b57fc256461f20e3600eff115b533bf307aa
Ben <bengedeon@icloud.com>
2023-03-15 11:17:20 +0100

    bisschen geändert

--- script.sh
+++ script.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+cd /home/dev/Dokumente/
+
 datei=find ./ -type f -name "*.sql"
 
 # shellcheck disable=SC1072
 while [ find ./ -type f -name "*.sql" ];
 do
-  gzip datei
-  if [ datei = *.gz ]
-    rm datei = *.sql
+  gzip $datei
+  if [ $datei = *.gz ]
+    rm $datei = *.sql
   fi
 done
