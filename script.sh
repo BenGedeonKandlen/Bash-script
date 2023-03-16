874fa720165e85257ec230134756bbdc5b547a33
Ben <bengedeon@icloud.com>
2023-03-16 08:40:25 +0100

    unnötiges echo entfernt

--- script.sh
+++ script.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-cd /home/dev/Dokumente
-
 for file in *.sql
 do
     if [[ -f $file ]]; then
-        gzip -9 $file
+        echo $file" wird komprimiert..."
+        gzip -9 -k $file
+        echo $file" ist fertig komprimiert"
     fi
 done
+
+read -p "Sollen die Original Dateien gelösch werden? -Ja[j] oder Nein[n]" antwort
+if [ $antwort == j ]; then
+    for file in *.sql
+       do
+           if [[ -f $file ]]; then
+               rm $file
+           fi
+       done
+fi
