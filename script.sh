1b02d4dcaf3a5babc2bb3ec5b16879e437f8118b
Ben <bengedeon@icloud.com>
2023-03-16 08:53:03 +0100

    weitere Frage eingebaut

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 for file in *.sql
 do
-    if [[ -f $file ]]; then
+    if [[ -f $file ]]; then       #läuft mit der Schleife alle .sql-Dateien durch.
         echo $file" wird komprimiert..."
         gzip -9 -k $file
         echo $file" ist fertig komprimiert"
@@ -12,9 +12,14 @@ done
 read -p "Sollen die Original Dateien gelösch werden? -Ja[j] oder Nein[n]" antwort
 if [ $antwort == j ]; then
     for file in *.sql
-       do
+       do     #wenn Ja[j] ausgewählt wird, dann erden die gefundenen Dateien gelöscht.
            if [[ -f $file ]]; then
                rm $file
            fi
        done
+fi
+
+read -p "Soll der Rechner heruntergefahren werden?  -Ja[j] oder Nein[n]" antwort2
+if [ $antwort2 == j ]; then
+    shudown
 fi
